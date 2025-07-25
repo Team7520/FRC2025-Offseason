ac1ba0829266f43f742c8eb0008569ac3584993f
Matthew Liang <woodyl_2009@hotmail.com>
2025-07-25 15:17:31 -0400

    Added support for USB Camera connected directly to roboRIO

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
@@ -53,6 +54,7 @@ public class Robot extends TimedRobot
     {
       DriverStation.silenceJoystickConnectionWarning(true);
     }
+    CameraServer.startAutomaticCapture();// Start the camera server to stream video to the dashboard.
   }
 
   /**
